f0e11e535e072ef134ecf68a3809aa14a82b1cb7
Alvaro Renan <alvaro.carvalho.703@ufrn.edu.br>
2022-06-26 23:53:17 -0300

    init

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const runner = document.querySelector('.runner');
+
+const jump = () => {
+    runner.classList.add('jump');
+    setTimeout(() => {
+        runner.classList.remove('jump');
+    },500);
+}
+
+document.addEventListener('keydown', jump);
