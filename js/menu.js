4594868fb6d0b9cd1a8b8b8be356d72218a87cf6
alvaro.carvalho.703 <alvaro.carvalho.703@ufrn.edu.br>
2022-06-28 06:11:50 -0300

    rascunho menu e level 1

---
+++ js/menu.js
@@ -0,0 +1,5 @@
+function instrucoes() {
+    var instrucoes = document.getElementById("instrucoes");
+    alert("APERTE O BOTÃO 'INICIAR' PARA COMEÇAR O JOGO");
+    instrucoes.style.display = "block";
+}
